000818c0a37b472109023c384a2e5de247b27308
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-06-22 20:13:16 +0000

    handled spaces

---
+++ check_spaces.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/** 
+ * check_spaces - checks if the user input only spaces.
+ * @line: The pointer to input string.
+ * Return: 1 on success, 0 on failure
+ */
+
+int check_spaces(char *line)
+{
+	int i = 0;
+
+	line[_strlen(line) - 1] = '\0';
+	while (line[i] != '\0')
+	{
+		if (line[i] != ' ')
+			return (0);
+		i++;
+
+	}
+	return (1);
+}
